ea3faaf608d32617b5fa736263f9569cedf89f28
Stéphane Graber <stgraber@stgraber.org>
2024-09-26 17:48:13 -0400

    incus: Add sysext image
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

---
+++ mkosi.images/incus/install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+[ "$1" = "final" ] || exit 0
+
+# Get the repository keyring key.
+if [ ! -e /etc/apt/keyrings/zabbly.asc ]; then
+    mkdir -p /etc/apt/keyrings/
+    curl -fsSL https://pkgs.zabbly.com/key.asc -o /etc/apt/keyrings/zabbly.asc
+fi
+
+# Add the repository.
+cat <<EOF > /etc/apt/sources.list.d/zabbly-incus-stable.sources
+Enabled: yes
+Types: deb
+URIs: https://pkgs.zabbly.com/incus/stable
+Suites: bookworm
+Components: main
+Architectures: amd64
+Signed-By: /etc/apt/keyrings/zabbly.asc
+
+EOF
+
+# Install the incus packages.
+apt-get update
+apt-get install incus incus-ui-canonical --yes
+
+exit 0
