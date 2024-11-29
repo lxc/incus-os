6b604494749fd2bbf545588260dfb299088f5293
Stéphane Graber <stgraber@stgraber.org>
2024-11-29 13:22:43 -0500

    mkosi: Update for current mkosi git
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- mkosi.images/base/mkosi.conf.d/20-incus-deps.sh
+++ mkosi.images/base/mkosi.conf.d/20-incus-deps.sh
@@ -1,24 +1,6 @@
 #!/bin/sh -eux
 [ "$1" = "final" ] || exit 0
 
-# Get the repository keyring key.
-if [ ! -e /etc/apt/keyrings/zabbly.asc ]; then
-    mkdir -p /etc/apt/keyrings/
-    curl -fsSL https://pkgs.zabbly.com/key.asc -o /etc/apt/keyrings/zabbly.asc
-fi
-
-# Add the repository.
-cat <<EOF > /etc/apt/sources.list.d/zabbly-incus-daily.sources
-Enabled: yes
-Types: deb
-URIs: https://pkgs.zabbly.com/incus/daily
-Suites: noble
-Components: main
-Architectures: amd64
-Signed-By: /etc/apt/keyrings/zabbly.asc
-
-EOF
-
 # Install the incus packages.
 apt-get update
 apt-get install incus incus-ui-canonical --yes
