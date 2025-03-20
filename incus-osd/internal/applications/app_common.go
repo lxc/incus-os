5310ab841f40d15fff9f8b82abf5c195af07c8ec
Stéphane Graber <stgraber@stgraber.org>
2025-03-20 16:11:56 -0400

    incus-osd: Introduce application-specific logic
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

---
+++ incus-osd/internal/applications/app_common.go
@@ -0,0 +1,17 @@
+package applications
+
+import (
+	"context"
+)
+
+type common struct{}
+
+// Start runs startup action.
+func (*common) Start(_ context.Context, _ string) error {
+	return nil
+}
+
+// Initialize runs first time initialization.
+func (*common) Initialize(_ context.Context) error {
+	return nil
+}
