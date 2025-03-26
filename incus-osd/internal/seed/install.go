7c9705c8c33ee1f59a56aee7865db9f3dc7a8f4e
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2025-03-26 09:13:53 -0600

    incus-osd/seed: Add InstallConfig
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

---
+++ incus-osd/internal/seed/install.go
@@ -0,0 +1,26 @@
+package seed
+
+// InstallConfig defines a struct to hold install configuration.
+type InstallConfig struct {
+	ForceInstall bool                 `json:"force_install" yaml:"forceInstall"` // If true, ignore any existing data on target install disk.
+	ForceReboot  bool                 `json:"force_reboot"  yaml:"forceReboot"`  // If true, reboot the system automatically upon completion rather than waiting for the install media to be removed.
+	Target       *InstallConfigTarget `json:"target"        yaml:"target"`       // Optional selector for the target install disk; if not set, expect a single drive to be present.
+}
+
+// InstallConfigTarget defines options used to select the target install disk.
+type InstallConfigTarget struct {
+	ID string `json:"id" yaml:"id"` // Name as listed in /dev/disk/by-id/, glob supported.
+}
+
+// GetInstallConfig extracts the list of applications from the seed data.
+func GetInstallConfig(partition string) (*InstallConfig, error) {
+	// Get the install configuration.
+	var config InstallConfig
+
+	err := parseFileContents(partition, "install", &config)
+	if err != nil {
+		return &InstallConfig{}, err
+	}
+
+	return &config, nil
+}
