d45691d5a067eea58bb90fd9293e31babe4b845c
Stéphane Graber <stgraber@stgraber.org>
2025-04-18 01:44:33 -0400

    incus-osd/applications: Add Stop function
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- incus-osd/internal/applications/app_common.go
+++ incus-osd/internal/applications/app_common.go
@@ -11,6 +11,11 @@ func (*common) Start(_ context.Context, _ string) error {
 	return nil
 }
 
+// Stop runs shutdown action.
+func (*common) Stop(_ context.Context, _ string) error {
+	return nil
+}
+
 // Initialize runs first time initialization.
 func (*common) Initialize(_ context.Context) error {
 	return nil
--- incus-osd/internal/applications/app_incus.go
+++ incus-osd/internal/applications/app_incus.go
@@ -16,6 +16,23 @@ func (*incus) Start(ctx context.Context, _ string) error {
 	return systemd.EnableUnit(ctx, true, "incus.socket", "incus-lxcfs.service", "incus-startup.service", "incus.service")
 }
 
+// Stop stops all the systemd units.
+func (*incus) Stop(ctx context.Context, _ string) error {
+	// Trigger a clean shutdown.
+	err := systemd.StopUnit(ctx, "incus-startup.service")
+	if err != nil {
+		return err
+	}
+
+	// Stop the remaining units.
+	err = systemd.StopUnit(ctx, "incus.service", "incus-lxcfs.service")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // Initialize runs first time initialization.
 func (*incus) Initialize(ctx context.Context) error {
 	// Get the preseed from the seed partition.
