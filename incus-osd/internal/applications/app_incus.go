b6c2a9e68f6b8e5d2e066a3c2b06ab7fefbf7c94
Stéphane Graber <stgraber@stgraber.org>
2025-04-26 01:11:32 -0400

    incus-osd/incus: Implement defaults
    Closes #64
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- incus-osd/internal/applications/app_incus.go
+++ incus-osd/internal/applications/app_incus.go
@@ -2,8 +2,10 @@ package applications
 
 import (
 	"context"
+	"fmt"
 
 	incusclient "github.com/lxc/incus/v6/client"
+	incusapi "github.com/lxc/incus/v6/shared/api"
 
 	"github.com/lxc/incus-os/incus-osd/internal/seed"
 	"github.com/lxc/incus-os/incus-osd/internal/systemd"
@@ -34,16 +36,18 @@ func (*incus) Stop(ctx context.Context, _ string) error {
 }
 
 // Initialize runs first time initialization.
-func (*incus) Initialize(ctx context.Context) error {
+func (a *incus) Initialize(ctx context.Context) error {
 	// Get the preseed from the seed partition.
-	incusPreseed, err := seed.GetIncus(ctx, seed.SeedPartitionPath)
+	incusSeed, err := seed.GetIncus(ctx, seed.SeedPartitionPath)
 	if err != nil && !seed.IsMissing(err) {
 		return err
 	}
 
-	// If no preseed, we're done.
-	if incusPreseed == nil {
-		return nil
+	// If no seed, build one for auto-configuration.
+	if incusSeed == nil {
+		incusSeed = &seed.IncusConfig{
+			ApplyDefaults: true,
+		}
 	}
 
 	// Connect to Incus.
@@ -52,10 +56,153 @@ func (*incus) Initialize(ctx context.Context) error {
 		return err
 	}
 
-	// Push the preseed.
-	err = c.ApplyServerPreseed(*incusPreseed)
+	// Push the preseed if one is present.
+	if incusSeed.Preseed != nil {
+		err = c.ApplyServerPreseed(*incusSeed.Preseed)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Handle the defaults.
+	if incusSeed.ApplyDefaults {
+		err = a.applyDefaults(c, incusSeed)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (*incus) applyDefaults(c incusclient.InstanceServer, incusSeed *seed.IncusConfig) error {
+	// Get server configuration.
+	serverConfig, serverConfigEtag, err := c.GetServer()
 	if err != nil {
 		return err
+	}
+
+	// Get the default profile.
+	profileDefault, profileDefaultEtag, err := c.GetProfile("default")
+	if err != nil {
+		return err
+	}
+
+	if profileDefault.Devices == nil {
+		profileDefault.Devices = map[string]map[string]string{}
+	}
+
+	// Check for storage pools.
+	storagePools, err := c.GetStoragePoolNames()
+	if err != nil {
+		return err
+	}
+
+	// Check for networks.
+	allNetworks, err := c.GetNetworks()
+	if err != nil {
+		return err
+	}
+
+	networks := []incusapi.Network{}
+	for _, network := range allNetworks {
+		if !network.Managed {
+			continue
+		}
+
+		networks = append(networks, network)
+	}
+
+	// Create storage pools.
+	if len(storagePools) == 0 {
+		// Create the local pool.
+		err = c.CreateStoragePool(incusapi.StoragePoolsPost{
+			Name:   "local",
+			Driver: "zfs",
+			StoragePoolPut: incusapi.StoragePoolPut{
+				Config: map[string]string{
+					"source": "local/incus",
+				},
+				Description: "Local storage pool (on system drive)",
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Create the default volumes.
+		for _, volName := range []string{"backups", "images"} {
+			// Create the volume.
+			err = c.CreateStoragePoolVolume("local", incusapi.StorageVolumesPost{
+				Name:        volName,
+				Type:        "custom",
+				ContentType: "filesystem",
+				StorageVolumePut: incusapi.StorageVolumePut{
+					Description: "Volume holding system " + volName,
+				},
+			})
+			if err != nil {
+				return err
+			}
+
+			// Make use of it.
+			serverConfig.Config[fmt.Sprintf("storage.%s_volume", volName)] = "local/" + volName
+		}
+
+		// Add to the default profile.
+		profileDefault.Devices["root"] = map[string]string{
+			"type": "disk",
+			"path": "/",
+			"pool": "local",
+		}
+	}
+
+	// Create networks.
+	if len(networks) == 0 {
+		// Create the incusbr0 network.
+		err = c.CreateNetwork(incusapi.NetworksPost{
+			Name: "incusbr0",
+			NetworkPut: incusapi.NetworkPut{
+				Description: "Local network bridge (NAT)",
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Add to the default profile.
+		profileDefault.Devices["eth0"] = map[string]string{
+			"type":    "nic",
+			"network": "incusbr0",
+			"name":    "eth0",
+		}
+	}
+
+	// Listen on the network by default.
+	_, ok := serverConfig.Config["core.https_address"]
+	if !ok {
+		serverConfig.Config["core.https_address"] = ":8443"
+	}
+
+	// Apply default profile changes.
+	err = c.UpdateProfile("default", profileDefault.Writable(), profileDefaultEtag)
+	if err != nil {
+		return err
+	}
+
+	// Apply server configuration.
+	err = c.UpdateServer(serverConfig.Writable(), serverConfigEtag)
+	if err != nil {
+		return err
+	}
+
+	// Enroll the extra certificates.
+	// NOTE: This is a temporary measure until we get https://github.com/lxc/incus/issues/1804.
+	for _, cert := range incusSeed.Certificates {
+		err = c.CreateCertificate(cert)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
--- incus-osd/internal/seed/incus.go
+++ incus-osd/internal/seed/incus.go
@@ -6,10 +6,22 @@ import (
 	incusapi "github.com/lxc/incus/v6/shared/api"
 )
 
+// IncusConfig is a wrapper around the Incus preseed.
+type IncusConfig struct {
+	Version string `json:"version" yaml:"version"`
+
+	ApplyDefaults bool `json:"apply_defaults" yaml:"apply_defaults"`
+
+	Preseed *incusapi.InitPreseed `json:"preseed" yaml:"preseed"`
+
+	// NOTE: Temporary until https://github.com/lxc/incus/issues/1804.
+	Certificates []incusapi.CertificatesPost `json:"certificates" yaml:"certificates"`
+}
+
 // GetIncus extracts the Incus preseed from the seed data.
-func GetIncus(_ context.Context, partition string) (*incusapi.InitPreseed, error) {
+func GetIncus(_ context.Context, partition string) (*IncusConfig, error) {
 	// Get the preseed.
-	var preseed incusapi.InitPreseed
+	var preseed IncusConfig
 
 	err := parseFileContents(partition, "incus", &preseed)
 	if err != nil {
