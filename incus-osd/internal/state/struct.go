76bf1ff7a1f6baf1d78cb19333575cb419834583
Stéphane Graber <stgraber@stgraber.org>
2025-06-14 15:03:32 -0400

    incus-osd/state: Add Hostname function
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- incus-osd/internal/state/struct.go
+++ incus-osd/internal/state/struct.go
@@ -1,6 +1,9 @@
 package state
 
 import (
+	"os"
+	"strings"
+
 	"github.com/lxc/incus-os/incus-osd/api"
 )
 
@@ -45,4 +48,34 @@ type State struct {
 		Network    api.SystemNetwork    `json:"network"`
 		Provider   api.SystemProvider   `json:"provider"`
 	} `json:"system"`
+}
+
+// Hostname returns the preferred hostname for the system.
+func (s *State) Hostname() string {
+	// Use the configured hostname if set by the user.
+	if s.System.Network.Config != nil && s.System.Network.Config.DNS != nil && s.System.Network.Config.DNS.Hostname != "" {
+		hostname := s.System.Network.Config.DNS.Hostname
+		if s.System.Network.Config.DNS.Domain != "" {
+			hostname += "." + s.System.Network.Config.DNS.Domain
+		}
+
+		return hostname
+	}
+
+	// Use product UUID if valid.
+	productUUID, err := os.ReadFile("/sys/class/dmi/id/product_uuid")
+	if err == nil && len(productUUID) == 37 {
+		// Got what should be a valid UUID, use that.
+		return strings.TrimSpace(string(productUUID))
+	}
+
+	// Use machine ID if valid.
+	machineID, err := os.ReadFile("/etc/machine-id")
+	if err == nil && len(machineID) == 33 {
+		// Got what should be a valid UUID, use that.
+		return strings.TrimSpace(string(machineID))
+	}
+
+	// If all else fails, use the OS name.
+	return s.OS.Name
 }
