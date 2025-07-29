f8fc376969e842ccf30390d939dcad3c0f48f67b
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2025-07-29 10:47:34 -0600

    incus-osd/storage: Move GetUnderlyingDevice() to storage package
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- incus-osd/internal/install/install.go
+++ incus-osd/internal/install/install.go
@@ -8,7 +8,6 @@ import (
 	"io"
 	"log/slog"
 	"os"
-	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
@@ -176,78 +175,12 @@ func runningFromCDROM() bool {
 		return false
 	}
 
-	underlyingDevice, err := getUnderlyingDevice()
+	underlyingDevice, err := storage.GetUnderlyingDevice()
 	if err != nil {
 		return false
 	}
 
 	return underlyingDevice == "/dev/sr0"
-}
-
-// getUnderlyingDevice figures out and returns the underlying device that Incus OS is running from.
-func getUnderlyingDevice() (string, error) {
-	// Determine the device we're running from.
-	s := unix.Stat_t{}
-
-	err := unix.Stat("/usr/local/bin/incus-osd", &s)
-	if err != nil {
-		return "", err
-	}
-
-	major := unix.Major(s.Dev)
-	minor := unix.Minor(s.Dev)
-	rootDev := fmt.Sprintf("%d:%d\n", major, minor)
-
-	// Get a list of all the block devices.
-	entries, err := os.ReadDir("/sys/class/block")
-	if err != nil {
-		return "", err
-	}
-
-	// Iterate through each of the block devices until we find the one for /usr.
-	for _, entry := range entries {
-		entryPath := filepath.Join("/sys/class/block", entry.Name())
-
-		dev, err := os.ReadFile(filepath.Join(entryPath, "dev")) //nolint:gosec
-		if err != nil {
-			continue
-		}
-
-		// We've found the mapped device.
-		if string(dev) == rootDev {
-			// Get the underlying device.
-			members, err := os.ReadDir(filepath.Join(entryPath, "slaves"))
-			if err != nil {
-				return "", err
-			}
-
-			// Read the symlink for the underlying device.
-			path, err := os.Readlink(filepath.Join(entryPath, "slaves", members[0].Name()))
-			if err != nil {
-				return "", err
-			}
-
-			// We're running from a USB stick.
-			if strings.HasPrefix(path, "../../../") {
-				// Drop the last element of the path (the partition), then get the base of the resulting path (the actual device).
-				parentDir, _ := filepath.Split(path)
-
-				return filepath.Join("/dev/", filepath.Base(parentDir)), nil
-			}
-
-			// We're running from a CDROM; need to do one more level of indirection to get the actual device.
-			entryPath = filepath.Join("/sys/class/block", filepath.Base(path))
-
-			members, err = os.ReadDir(filepath.Join(entryPath, "slaves"))
-			if err != nil {
-				return "", err
-			}
-
-			return filepath.Join("/dev/", members[0].Name()), nil
-		}
-	}
-
-	return "", errors.New("unable to determine underlying device")
 }
 
 // getSourceDevice determines the underlying device incus-osd is running on and if it is read-only.
@@ -268,7 +201,7 @@ func getSourceDevice(ctx context.Context) (string, bool, error) {
 
 	isReadonlyInstallFS := strings.Contains(output, "Dependency failed for boot.mount - EFI System Partition Automount.")
 
-	underlyingDevice, err := getUnderlyingDevice()
+	underlyingDevice, err := storage.GetUnderlyingDevice()
 	if err != nil {
 		return "", isReadonlyInstallFS, err
 	}
