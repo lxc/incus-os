acae87bcfa3389dbff81dc26b7f048bf614666ca
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2025-08-13 10:27:00 -0600

    incus-osd/state: Remove legacy json state support
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- incus-osd/internal/state/state_test.go
+++ incus-osd/internal/state/state_test.go
@@ -1,7 +1,6 @@
 package state_test
 
 import (
-	"encoding/json"
 	"strings"
 	"testing"
 
@@ -9,8 +8,6 @@ import (
 
 	"github.com/lxc/incus-os/incus-osd/internal/state"
 )
-
-var goldJSON = `{"secure_boot":{"version":"","fully_applied":false},"applications":{"incus":{"initialized":true,"version":"202506241635"}},"os":{"name":"IncusOS","running_release":"202506241635","next_release":"202506241635"},"services":{"iscsi":{"state":{"initiator_name":""},"config":{"enabled":false,"targets":null}},"lvm":{"state":{},"config":{"enabled":false,"system_id":0}},"multipath":{"config":{"enabled":false,"wwns":null},"state":{"devices":null}},"nvme":{"state":{"host_id":"","host_nqn":""},"config":{"enabled":false,"targets":null}},"ovn":{"state":{},"config":{"enabled":false,"ic_chassis":false,"database":"","tls_client_certificate":"","tls_client_key":"","tls_ca_certificate":"","tunnel_address":"","tunnel_protocol":""}},"usbip":{"state":{},"config":{"targets":null}}},"system":{"security":{"config":{"encryption_recovery_keys":["ebbbibiu-ltgjfuhk-gvutdrvu-hijhvfje-gvlrgrfv-ndekdtdh-ghteuklj-ldedfifb"]},"state":{"encrypted_volumes":[],"encryption_recovery_keys_retrieved":true,"secure_boot_certificates":[],"secure_boot_enabled":false,"tpm_status":"","pool_recovery_keys":{}}},"network":{"config":{"interfaces":[{"name":"enp5s0","addresses":["dhcp4","slaac"],"hwaddr":"10:66:6a:7c:8c:b0","lldp":false}]},"state":{"interfaces":{"enp5s0":{"type":"interface","hwaddr":"10:66:6a:7c:8c:b0","addresses":["10.234.136.156"],"routes":[{"to":"default","via":"10.234.136.1"}],"mtu":1500,"speed":"10Gbps","state":"routable","stats":{"rx_bytes":944,"tx_bytes":751,"rx_errors":0,"tx_errors":0}}}}},"provider":{"config":{"name":"local","config":{"multiline_value":"first\nsecond\nthird"}},"state":{"registered":false}}}}`
 
 var goldEncodingV0 = `#Version: 0
 Applications[incus].Initialized: true
@@ -44,21 +41,6 @@ System.Provider.Config.Name: local
 System.Provider.Config.Config[multiline_value]: first\nsecond\nthird
 `
 
-// Test basic json decoding/encoding of state.
-func TestJsonEncoding(t *testing.T) {
-	t.Parallel()
-
-	var s state.State
-
-	err := json.Unmarshal([]byte(goldJSON), &s)
-	require.NoError(t, err)
-
-	content, err := json.Marshal(s)
-	require.NoError(t, err)
-
-	require.JSONEq(t, goldJSON, string(content))
-}
-
 // Test basic custom decoding/encoding of state.
 func TestCustomEncoding(t *testing.T) {
 	t.Parallel()
@@ -75,29 +57,6 @@ func TestCustomEncoding(t *testing.T) {
 	require.Equal(t, 1, s.StateVersion)
 
 	require.Equal(t, 2, strings.Count(s.System.Provider.Config.Config["multiline_value"], "\n"))
-}
-
-// Test that we can correctly read in the json format and produce the expected custom encoding.
-func TestEncodingSwitch(t *testing.T) {
-	t.Parallel()
-
-	var js state.State
-
-	err := json.Unmarshal([]byte(goldJSON), &js)
-	require.NoError(t, err)
-
-	content, err := state.Encode(&js)
-	require.NoError(t, err)
-
-	var cs1, cs2 state.State
-
-	err = state.Decode(content, nil, &cs1)
-	require.NoError(t, err)
-
-	err = state.Decode([]byte(goldEncodingV1), nil, &cs2)
-	require.NoError(t, err)
-
-	require.Equal(t, cs1, cs2)
 }
 
 // Test simple upgrade functions when reading in state.
