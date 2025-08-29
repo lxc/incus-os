352d8c1f10d8d2d9eaf7e2933e700df50fb6ccc9
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2025-08-29 14:30:22 -0600

    incus-osd/api: Define a custom Weekday type
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

---
+++ incus-osd/api/weekday.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"time"
+)
+
+// Weekday defines our own type. The time package's Weekday doesn't include any way to indicate
+// an empty value, which we need.
+type Weekday string
+
+// Names of each day of the week.
+const (
+	NONE      Weekday = ""
+	Sunday    Weekday = "Sunday"
+	Monday    Weekday = "Monday"
+	Tuesday   Weekday = "Tuesday"
+	Wednesday Weekday = "Wednesday"
+	Thursday  Weekday = "Thursday"
+	Friday    Weekday = "Friday"
+	Saturday  Weekday = "Saturday"
+)
+
+// ToWeekday converts our string representation into the time package's int-based Weekeday.
+// It is assumed the value has already been checked to be a valid weekday, as no such
+// error checking is performed here.
+func (w Weekday) ToWeekday() time.Weekday {
+	switch w {
+	case Sunday:
+		return time.Sunday
+	case Monday:
+		return time.Monday
+	case Tuesday:
+		return time.Tuesday
+	case Wednesday:
+		return time.Wednesday
+	case Thursday:
+		return time.Thursday
+	case Friday:
+		return time.Friday
+	case Saturday:
+		return time.Saturday
+	case NONE:
+		return -1
+	default:
+		return -1
+	}
+}
