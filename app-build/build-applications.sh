baed8462743a7d024c4e3048e0aa412fc1d58c3c
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2025-09-04 09:42:41 -0600

    Add Operations Center application layer
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- app-build/build-applications.sh
+++ app-build/build-applications.sh
@@ -3,7 +3,38 @@
 set -e
 
 ### Versions ###
+INCUS_TERRAFORM_VERSION=0.5.0
 KPX_VERSION=1.12.1
+OPENTOFU_VERSION=1.10.6
+OPERATIONS_CENTER_VERSION=main
+
+### Incus terraform/tofu plugin ###
+if [ -d terraform-provider-incus ]; then
+    pushd terraform-provider-incus
+    git reset --hard && git fetch --depth 1 origin "v${INCUS_TERRAFORM_VERSION}:refs/tags/v${INCUS_TERRAFORM_VERSION}" && git checkout "v${INCUS_TERRAFORM_VERSION}"
+    popd
+else
+    git clone https://github.com/lxc/terraform-provider-incus.git terraform-provider-incus --depth 1 -b "v${INCUS_TERRAFORM_VERSION}"
+fi
+
+    pushd terraform-provider-incus
+    go build .
+    strip terraform-provider-incus
+
+    # tofu expects the provider to be in a versioned path. To make the copy logic easier in the Makefile, construct the versioned path here.
+    ARCH=$(uname -m)
+    if [ "${ARCH}" = "aarch64" ]; then
+        ARCH="arm64"
+    elif [ "${ARCH}" = "x86_64" ]; then
+        ARCH="amd64"
+    else
+        die "Unsupported architecture: ${ARCH}"
+    fi
+
+    rm -rf lxc/
+    mkdir -p "lxc/incus/${INCUS_TERRAFORM_VERSION}/linux_${ARCH}"
+    mv terraform-provider-incus "lxc/incus/${INCUS_TERRAFORM_VERSION}/linux_${ARCH}/terraform-provider-incus_v${INCUS_TERRAFORM_VERSION}"
+    popd
 
 ### kpx ###
 if [ -d kpx ]; then
@@ -19,4 +50,37 @@ fi
 
     go build -o kpx -ldflags="s -w -X github.com/momiji/kpx.AppVersion=${KPX_VERSION}" ./cli
     strip kpx
+    popd
+
+### opentofu ###
+if [ -d opentofu ]; then
+    pushd opentofu
+    git reset --hard && git fetch --depth 1 origin "v${OPENTOFU_VERSION}:refs/tags/v${OPENTOFU_VERSION}" && git checkout "v${OPENTOFU_VERSION}"
+    popd
+else
+    git clone https://github.com/opentofu/opentofu.git opentofu --depth 1 -b "v${OPENTOFU_VERSION}"
+fi
+
+    pushd opentofu
+    go build -o tofu ./cmd/tofu
+    strip tofu
+    popd
+
+### Operations Center ###
+if [ -d operations-center ]; then
+    pushd operations-center
+    git reset --hard && git pull
+    popd
+else
+    git clone https://github.com/FuturFusion/operations-center.git operations-center --depth 1 -b "${OPERATIONS_CENTER_VERSION}"
+fi
+
+    pushd operations-center
+    go build -o operations-centerd ./cmd/operations-centerd
+    strip operations-centerd
+
+    pushd ui
+    YARN_ENABLE_HARDENED_MODE=0 YARN_ENABLE_IMMUTABLE_INSTALLS=false yarnpkg install && yarnpkg build
+    popd
+
     popd
