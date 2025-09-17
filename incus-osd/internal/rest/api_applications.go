bc95b9d638355f3a455150e78389dcde1bc7b871
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2025-09-17 11:25:14 -0600

    incus-osd/rest: Add a DELETE applications endpoint to trigger app uninstall
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- incus-osd/internal/rest/api_applications.go
+++ incus-osd/internal/rest/api_applications.go
@@ -1,10 +1,15 @@
 package rest
 
 import (
+	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
 	"net/url"
 	"slices"
 
+	"github.com/lxc/incus-os/incus-osd/api"
+	"github.com/lxc/incus-os/incus-osd/internal/applications"
 	"github.com/lxc/incus-os/incus-osd/internal/rest/response"
 )
 
@@ -41,12 +46,6 @@ func (s *Server) apiApplications(w http.ResponseWriter, r *http.Request) {
 func (s *Server) apiApplicationsEndpoint(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	if r.Method != http.MethodGet {
-		_ = response.NotImplemented(nil).Render(w)
-
-		return
-	}
-
 	name := r.PathValue("name")
 
 	// Check if the application is valid.
@@ -57,6 +56,61 @@ func (s *Server) apiApplicationsEndpoint(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	// Handle the request.
-	_ = response.SyncResponse(true, app).Render(w)
+	switch r.Method {
+	case http.MethodGet:
+		// Handle the request.
+		_ = response.SyncResponse(true, app).Render(w)
+	case http.MethodDelete:
+		// Uninstall the application.
+		if r.ContentLength <= 0 {
+			_ = response.BadRequest(errors.New("no uninstall parameters provided")).Render(w)
+
+			return
+		}
+
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			_ = response.BadRequest(err).Render(w)
+
+			return
+		}
+
+		deleteStruct := api.ApplicationDelete{}
+
+		err = json.Unmarshal(b, &deleteStruct)
+		if err != nil {
+			_ = response.BadRequest(err).Render(w)
+
+			return
+		}
+
+		actualApp, err := applications.Load(r.Context(), name)
+		if err != nil {
+			_ = response.BadRequest(err).Render(w)
+
+			return
+		}
+
+		err = actualApp.Uninstall(r.Context(), deleteStruct.RemoveUserData)
+		if err != nil {
+			_ = response.BadRequest(err).Render(w)
+
+			return
+		}
+
+		// Remove the application from state and then write to disk.
+		delete(s.state.Applications, name)
+
+		err = s.state.Save(r.Context())
+		if err != nil {
+			_ = response.BadRequest(err).Render(w)
+
+			return
+		}
+
+		_ = response.EmptySyncResponse.Render(w)
+	default:
+		// If none of the supported methods, return NotImplemented.
+		_ = response.NotImplemented(nil).Render(w)
+	}
 }
