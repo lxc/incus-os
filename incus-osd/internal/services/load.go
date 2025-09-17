23de0187ed442bacb668955f4daf3416c90d0e0d
Stéphane Graber <stgraber@stgraber.org>
2025-09-17 14:14:34 -0400

    incus-osd/services: Allow services to declare if supported on the system or not
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- incus-osd/internal/services/load.go
+++ incus-osd/internal/services/load.go
@@ -3,16 +3,49 @@ package services
 import (
 	"context"
 	"errors"
+	"slices"
 
 	"github.com/lxc/incus-os/incus-osd/internal/state"
 )
 
-// Supported contains the list of all valid services.
+// Supported returns the list of all valid services for this system.
 // The list is sorted in recommended startup order to handle service dependencies.
-var Supported = []string{"ceph", "iscsi", "nvme", "multipath", "lvm", "ovn", "usbip"}
+func Supported(s *state.State) []string {
+	services := []string{"ceph", "iscsi", "nvme", "multipath", "lvm", "ovn", "usbip"}
+	supported := make([]string, 0, len(services))
+
+	for _, service := range services {
+		srv, err := loadByName(s, service)
+		if err != nil {
+			continue
+		}
+
+		if !srv.Supported() {
+			continue
+		}
+
+		supported = append(supported, service)
+	}
+
+	return supported
+}
 
 // Load returns a handler for the given system service.
-func Load(ctx context.Context, s *state.State, name string) (Service, error) {
+func Load(_ context.Context, s *state.State, name string) (Service, error) {
+	if !slices.Contains(Supported(s), name) {
+		return nil, errors.New("unknown service")
+	}
+
+	// Load the service.
+	srv, err := loadByName(s, name)
+	if err != nil {
+		return nil, err
+	}
+
+	return srv, nil
+}
+
+func loadByName(s *state.State, name string) (Service, error) {
 	var srv Service
 
 	switch name {
