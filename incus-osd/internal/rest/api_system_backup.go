3aa42738433ffe155df54015500e648bb2655167
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2025-09-25 11:51:35 -0600

    incus-osd/rest: Add a system/backup REST endpoint
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

---
+++ incus-osd/internal/rest/api_system_backup.go
@@ -0,0 +1,72 @@
+package rest
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/lxc/incus-os/incus-osd/internal/backup"
+	"github.com/lxc/incus-os/incus-osd/internal/rest/response"
+)
+
+func (s *Server) apiSystemBackup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method != http.MethodPost {
+		_ = response.NotImplemented(nil).Render(w)
+
+		return
+	}
+
+	// Make sure we have the current state written to disk prior to backup.
+	err := s.state.Save()
+	if err != nil {
+		_ = response.BadRequest(err).Render(w)
+
+		return
+	}
+
+	archive, err := backup.GetOSBackup()
+	if err != nil {
+		_ = response.BadRequest(err).Render(w)
+
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/x-tar")
+
+	_, err = w.Write(archive)
+	if err != nil {
+		_ = response.BadRequest(err).Render(w)
+
+		return
+	}
+}
+
+func (s *Server) apiSystemRestore(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method != http.MethodPost {
+		_ = response.NotImplemented(nil).Render(w)
+
+		return
+	}
+
+	if r.ContentLength <= 0 {
+		_ = response.BadRequest(errors.New("no tar archive provided")).Render(w)
+
+		return
+	}
+
+	skipString := r.FormValue("skip")
+	skip := strings.Split(skipString, ",")
+
+	err := backup.ApplyOSBackup(r.Context(), s.state, r.Body, skip)
+	if err != nil {
+		_ = response.BadRequest(err).Render(w)
+
+		return
+	}
+
+	_ = response.EmptySyncResponse.Render(w)
+}
