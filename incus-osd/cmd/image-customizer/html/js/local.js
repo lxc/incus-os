a7c0077b87a839ae62fdcd27513a2b58d97db047
Stéphane Graber <stgraber@stgraber.org>
2025-10-07 16:27:52 -0400

    image-customizer: Javascript lint
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- incus-osd/cmd/image-customizer/html/js/local.js
+++ incus-osd/cmd/image-customizer/html/js/local.js
@@ -1,100 +1,103 @@
 function download() {
+    req = {
+        "seeds": {}
+    };
+
     // Validate image type.
     if (document.getElementById("imageTypeISO").checked) {
-        req["type"] = "iso"
+        req.type = "iso";
     } else if (document.getElementById("imageTypeUSB").checked) {
-        req["type"] = "raw"
+        req.type = "raw";
     } else {
-        alert("Missing image type")
-        return
+        alert("Missing image type");
+        return;
     }
 
     // Validate image architecture.
     if (document.getElementById("imageArchitectureX86_64").checked) {
-        req["architecture"] = "x86_64"
+        req.architecture = "x86_64";
     } else if (document.getElementById("imageArchitectureAARCH64").checked) {
-        req["architecture"] = "aarch64"
+        req.architecture = "aarch64";
     } else {
-        alert("Missing image architecture")
-        return
+        alert("Missing image architecture");
+        return;
     }
 
     // Validate image application.
-    app = ""
-    if (document.getElementById("imageAppIncus").checked) {
-        app = "incus"
-    } else if (document.getElementById("imageAppOperationsCenter").checked) {
-        app = "operations-center"
-    } else if (document.getElementById("imageAppMigrationManager").checked) {
-        app = "migration-manager"
+    app = "";
+    if (document.getElementById("imageApplicationIncus").checked) {
+        app = "incus";
+    } else if (document.getElementById("imageApplicationOperationsCenter").checked) {
+        app = "operations-center";
+    } else if (document.getElementById("imageApplicationMigrationManager").checked) {
+        app = "migration-manager";
     } else {
-        alert("Missing image application")
-        return
+        alert("Missing image application");
+        return;
     }
 
     // Validate client certificate.
-    if (document.getElementById("appClientCertificate").value == "") {
-        alert("Missing client certificate")
-        return
+    if (document.getElementById("applicationClientCertificate").value == "") {
+        alert("Missing client certificate");
+        return;
     }
 
     // Generate installation seed.
-    req = {}
-    req["seeds"] = {}
-
     if (document.getElementById("imageUsageInstallation").checked) {
-        install = {}
-        install["version"] = "1"
+        install = {
+            "version": "1"
+        };
 
         if (document.getElementById("imageForceInstall").checked) {
-            install["force_install"] = true
+            install.force_install = true;
         }
 
         if (document.getElementById("imageForceReboot").checked) {
-            install["force_reboot"] = true
+            install.force_reboot = true;
         }
 
         if (document.getElementById("imageInstallTarget").value != "") {
-            install["target"] = {}
-            install["target"]["id"] = document.getElementById("imageInstallTarget").value
+            install.target = {
+                "id": document.getElementById("imageInstallTarget").value
+            };
         }
 
-        req["seeds"]["install"] = install
+        req.seeds.install = install;
     }
 
     // Generate application seed.
-    req["seeds"]["applications"] = {
+    req.seeds.applications = {
        "version": "1",
        "applications": [{"name": app}]
-    }
+    };
 
-    if app == "incus" {
+    if (app == "incus") {
         certificate = {
             "name": "admin",
             "type": "client",
             "description": "Initial admin client",
-            "certificate": document.getElementById("incusClientCertificate").value
-        }
+            "certificate": document.getElementById("applicationClientCertificate").value
+        };
 
         incus = {
             "version": "1",
             "preseed": {
                 "certificates": [certificate]
             }
+        };
+
+        if (document.getElementById("applicationDefaults").checked) {
+            incus.apply_defaults = true;
         }
 
-        if (document.getElementById("appDefaults").checked) {
-            incus["apply_defaults"] = true
-        }
-
-        req["seeds"]["incus"] = incus
+        req.seeds.incus = incus;
     } else {
         appSeed = {
             "version": "1",
-            "trusted_client_certificates": [certificate]
-        }
+            "trusted_client_certificates": [document.getElementById("applicationClientCertificate").value]
+        };
 
-        req["seeds"][app] = appSeed
+        req.seeds[app] = appSeed;
     }
 
     // Send the request.
@@ -105,11 +108,11 @@ function download() {
             "Content-Type": "application/json"
         }
     }).then(response => response.json()).then(function(response) {
-        if (response["status_code"] != 200) {
-            alert("Unable to generate the requested image")
-            return
+        if (response.status_code != 200) {
+            alert("Unable to generate the requested image");
+            return;
         }
 
-        window.location.href = document.location.origin+response["metadata"]["image"];
-    })
+        window.location.href = document.location.origin+response.metadata.image;
+    });
 }
