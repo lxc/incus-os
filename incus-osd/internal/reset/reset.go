c48da8e4f0146a18c5217ddbcebf5a1b98d1b2fb
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2025-10-10 08:08:13 -0600

    incus-osd/reset: Extend reset API struct with a couple more options
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- incus-osd/internal/reset/reset.go
+++ incus-osd/internal/reset/reset.go
@@ -5,6 +5,8 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"os"
 
 	"github.com/lxc/incus/v6/shared/subprocess"
@@ -41,7 +43,7 @@ func PerformOSFactoryReset(ctx context.Context, resetSeed *api.SystemReset) erro
 	// Verify any provided seed data is valid json.
 	type empty struct{}
 
-	for seed, seedData := range resetSeed.Seed {
+	for seed, seedData := range resetSeed.Seeds {
 		if seedData == nil {
 			return errors.New("seed data for '" + seed + "' is not defined")
 		}
@@ -54,24 +56,39 @@ func PerformOSFactoryReset(ctx context.Context, resetSeed *api.SystemReset) erro
 		}
 	}
 
-	// First, wipe the TPM.
-	_, err = subprocess.RunCommandContext(ctx, "tpm2_clear")
-	if err != nil {
-		return err
-	}
+	// First, write the seed data. Do this first, since any errors encountered won't cause
+	// the currently running system to break.
+	partitionPrefix := install.GetPartitionPrefix(underlyingDevice)
+	seedPartition := underlyingDevice + partitionPrefix + "2"
+	seeds := make(map[string][]byte)
 
-	// Second, wipe system partitions (swap, root, and local-data).
-	for _, partitionIndex := range []string{"9", "10", "11"} {
-		_, err := subprocess.RunCommandContext(ctx, "sgdisk", "-d", partitionIndex, underlyingDevice)
+	if !resetSeed.WipeExistingSeeds {
+		// Read any existing seed data and augment it with provided seed update(s), if any.
+		existingSeeds, err := getExistingSeeds(seedPartition)
 		if err != nil {
 			return err
 		}
+
+		for seed, seedData := range resetSeed.Seeds {
+			// Clear any existing data for the new seed.
+			for _, ext := range []string{".json", ".yaml", ".yml"} {
+				delete(existingSeeds, seed+ext)
+			}
+
+			// Copy seed data and set appropriate file extension.
+			existingSeeds[seed+".json"] = seedData
+		}
+
+		seeds = existingSeeds
+	} else {
+		for seed, seedData := range resetSeed.Seeds {
+			// Copy seed data and set appropriate file extension.
+			seeds[seed+".json"] = seedData
+		}
 	}
 
-	// Third, write the seed data.
-	partitionPrefix := install.GetPartitionPrefix(underlyingDevice)
 	// #nosec G304
-	f, err := os.Create(underlyingDevice + partitionPrefix + "2")
+	f, err := os.Create(seedPartition)
 	if err != nil {
 		return err
 	}
@@ -79,9 +96,9 @@ func PerformOSFactoryReset(ctx context.Context, resetSeed *api.SystemReset) erro
 
 	tw := tar.NewWriter(f)
 
-	for seed, seedData := range resetSeed.Seed {
+	for seed, seedData := range seeds {
 		header := &tar.Header{
-			Name: seed + ".json",
+			Name: seed,
 			Mode: 0o600,
 			Size: int64(len(seedData)),
 		}
@@ -102,6 +119,63 @@ func PerformOSFactoryReset(ctx context.Context, resetSeed *api.SystemReset) erro
 		return err
 	}
 
+	// Beyond this point, we start making destructive changes to the system.
+	// If an error is encountered, we'll likely end up with a bricked system.
+
+	// Second, wipe the TPM.
+	_, err = subprocess.RunCommandContext(ctx, "tpm2_clear")
+	if err != nil {
+		// Some systems return errors when trying to clear the TPM. As a workaround,
+		// allow the user to indicate we should accept this error and continue.
+		if !resetSeed.AllowTPMResetFailure {
+			return err
+		}
+	}
+
+	// Third, wipe system partitions (swap, root, and local-data).
+	for _, partitionIndex := range []string{"9", "10", "11"} {
+		_, err := subprocess.RunCommandContext(ctx, "sgdisk", "-d", partitionIndex, underlyingDevice)
+		if err != nil {
+			return err
+		}
+	}
+
 	// Finally, immediately reboot the system.
 	return systemd.SystemReboot(ctx)
+}
+
+func getExistingSeeds(seedPartition string) (map[string][]byte, error) {
+	ret := make(map[string][]byte)
+
+	// #nosec G304
+	f, err := os.Open(seedPartition)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	tr := tar.NewReader(f)
+	for {
+		header, err := tr.Next()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			return nil, err
+		}
+
+		ret[header.Name] = make([]byte, header.Size)
+
+		bytesRead, err := tr.Read(ret[header.Name])
+		if err != nil && !errors.Is(err, io.EOF) {
+			return nil, err
+		}
+
+		if int64(bytesRead) != header.Size {
+			return nil, fmt.Errorf("only read %d of %d bytes for existing seed file '%s'", bytesRead, header.Size, header.Name)
+		}
+	}
+
+	return ret, nil
 }
