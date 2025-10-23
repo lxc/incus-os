3f412d67a0513da1145c28489764461950faaa1c
Stéphane Graber <stgraber@stgraber.org>
2025-10-23 19:44:12 -0400

    incus-osd/applications: Sort Application interface
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- incus-osd/internal/applications/struct.go
+++ incus-osd/internal/applications/struct.go
@@ -8,16 +8,16 @@ import (
 
 // Application represents an installed application.
 type Application interface { //nolint:interfacebloat
-	GetCertificate() (*tls.Certificate, error)
 	AddTrustedCertificate(ctx context.Context, name string, cert string) error
+	FactoryReset(ctx context.Context) error
+	GetBackup(archive io.Writer, complete bool) error
+	GetCertificate() (*tls.Certificate, error)
 	Initialize(ctx context.Context) error
 	IsPrimary() bool
 	IsRunning(ctx context.Context) bool
+	RestoreBackup(archive io.Reader) error
 	Start(ctx context.Context, version string) error
 	Stop(ctx context.Context, version string) error
 	Update(ctx context.Context, version string) error
 	WipeLocalData() error
-	FactoryReset(ctx context.Context) error
-	GetBackup(archive io.Writer, complete bool) error
-	RestoreBackup(archive io.Reader) error
 }
