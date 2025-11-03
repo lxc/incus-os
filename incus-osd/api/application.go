0f088d3bb1054019dbcd85338b7970bc63a21217
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2025-11-03 12:51:05 -0700

    incus-osd/api: Add LastRestored field to track when an application was restored
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- incus-osd/api/application.go
+++ incus-osd/api/application.go
@@ -1,10 +1,15 @@
 package api
 
+import (
+	"time"
+)
+
 // Application represents the state and configuration of a generic application.
 type Application struct {
 	State struct {
-		Initialized bool   `json:"initialized" yaml:"initialized"`
-		Version     string `json:"version"     yaml:"version"`
+		Initialized  bool       `json:"initialized"             yaml:"initialized"`
+		Version      string     `json:"version"                 yaml:"version"`
+		LastRestored *time.Time `json:"last_restored,omitempty" yaml:"last_restored,omitempty"`
 	} `json:"state" yaml:"state"`
 
 	Config struct{} `json:"config" yaml:"config"`
