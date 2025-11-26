05903eef72b076ab3a3de7f38a2b818d23605251
Stéphane Graber <stgraber@stgraber.org>
2025-11-26 15:21:03 -0500

    image-customizer: Add certificate generation API
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- incus-osd/cmd/image-customizer/main.go
+++ incus-osd/cmd/image-customizer/main.go
@@ -5,8 +5,16 @@ import (
 	"archive/tar"
 	"compress/gzip"
 	"context"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
 	"embed"
+	"encoding/base64"
 	"encoding/json"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"io"
@@ -24,6 +32,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/timpalpant/gzran"
 	"gopkg.in/yaml.v3"
+	"software.sslmate.com/src/go-pkcs12"
 
 	apiupdate "github.com/lxc/incus-os/incus-osd/api/images"
 	apiseed "github.com/lxc/incus-os/incus-osd/api/seed"
@@ -45,6 +54,12 @@ var (
 	images   map[string]apiImagesPost
 	imagesMu sync.Mutex
 )
+
+type apiCertificateGet struct {
+	Certificate string `json:"certificate"`
+	Key         string `json:"key"`
+	PFX         string `json:"pfx"`
+}
 
 type apiImagesPost struct {
 	Architecture string             `json:"architecture" yaml:"architecture"`
@@ -98,6 +113,7 @@ func do(ctx context.Context) error {
 
 	router.HandleFunc("/", apiRoot)
 	router.HandleFunc("/1.0", apiRoot10)
+	router.HandleFunc("/1.0/certificate", apiCertificate)
 	router.HandleFunc("/1.0/images", apiImages)
 	router.HandleFunc("/1.0/images/{uuid}", apiImage)
 	router.Handle("/ui/", http.StripPrefix("/ui/", http.FileServer(http.FS(fsUI))))
@@ -156,6 +172,83 @@ func apiRoot10(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = response.SyncResponse(true, map[string]any{}).Render(w)
+}
+
+func apiCertificate(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method != http.MethodGet {
+		_ = response.NotImplemented(nil).Render(w)
+
+		return
+	}
+
+	// Generate the certificate.
+	key, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	if err != nil {
+		_ = response.BadRequest(err).Render(w)
+
+		return
+	}
+
+	certTemplate := x509.Certificate{
+		Subject: pkix.Name{
+			Organization: []string{"Linux Containers"},
+			CommonName:   "Auto-generated IncusOS client certificate",
+		},
+
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().Add(10 * 365 * 24 * time.Hour),
+
+		KeyUsage:    x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+
+		BasicConstraintsValid: true,
+		DNSNames:              []string{"unspecified"},
+	}
+
+	certDerBytes, err := x509.CreateCertificate(rand.Reader, &certTemplate, &certTemplate, &key.PublicKey, key)
+	if err != nil {
+		_ = response.BadRequest(err).Render(w)
+
+		return
+	}
+
+	keyDerBytes, err := x509.MarshalECPrivateKey(key)
+	if err != nil {
+		_ = response.BadRequest(err).Render(w)
+
+		return
+	}
+
+	certBytes := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDerBytes})
+	keyBytes := pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: keyDerBytes})
+
+	// Load the cert and key.
+	cert, err := tls.X509KeyPair(certBytes, keyBytes)
+	if err != nil {
+		_ = response.BadRequest(err).Render(w)
+
+		return
+	}
+
+	// Get the PKCS12.
+	pfx, err := pkcs12.Modern2023.Encode(cert.PrivateKey, cert.Leaf, nil, "IncusOS")
+	if err != nil {
+		_ = response.BadRequest(err).Render(w)
+
+		return
+	}
+
+	slog.Info("certificate generated", "client", clientAddress(r))
+
+	resp := apiCertificateGet{
+		Certificate: string(certBytes),
+		Key:         string(keyBytes),
+		PFX:         base64.StdEncoding.EncodeToString(pfx),
+	}
+
+	_ = response.SyncResponse(true, resp).Render(w)
 }
 
 func apiImages(w http.ResponseWriter, r *http.Request) {
--- incus-osd/go.mod
+++ incus-osd/go.mod
@@ -89,4 +89,5 @@ require (
 	golang.org/x/term v0.36.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
 	google.golang.org/protobuf v1.36.10 // indirect
+	software.sslmate.com/src/go-pkcs12 v0.6.0 // indirect
 )
