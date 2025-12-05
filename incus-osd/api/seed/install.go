dd01353279033ee5b33688015fe129d51b72eb0f
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2025-12-05 07:37:09 -0700

    incus-osd/api/seed: Add UseSWTPM field to install seed
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- incus-osd/api/seed/install.go
+++ incus-osd/api/seed/install.go
@@ -6,6 +6,7 @@ type Install struct {
 
 	ForceInstall bool           `json:"force_install" yaml:"force_install"` // If true, ignore any existing data on target install disk.
 	ForceReboot  bool           `json:"force_reboot"  yaml:"force_reboot"`  // If true, reboot the system automatically upon completion rather than waiting for the install media to be removed.
+	UseSWTPM     bool           `json:"use_swtpm"     yaml:"use_swtpm"`     // If true, and only if no physical TPM is present, allow fallback to swtpm-backed TPM implementation. !!THIS WILL REDUCE THE SYSTEM'S SECURITY COMPARED TO USING A PROPER TPM!!
 	Target       *InstallTarget `json:"target"        yaml:"target"`        // Optional selector for the target install disk; if not set, expect a single drive to be present.
 }
 
