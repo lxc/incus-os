19cecdac5792aa70c22dc5d4181a5bb16190570e
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2025-12-08 15:59:16 -0700

    incus-osd/tests: Add tests for swtpm
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- incus-osd/tests/incusos_tests/tests_incusos_live.py
+++ incus-osd/tests/incusos_tests/tests_incusos_live.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 from .incus_test_vm import IncusTestVM, util
 
@@ -20,5 +21,36 @@ def TestIncusOSLive(install_image):
         vm.StartVM()
         vm.WaitAgentRunning()
         vm.WaitExpectedLog("incus-osd", "Auto-generating encryption recovery key, this may take a few seconds")
+        vm.WaitExpectedLog("incus-osd", "Downloading application update application=incus version="+incusos_version)
+        vm.WaitExpectedLog("incus-osd", "System is ready version="+incusos_version)
+
+def TestIncusOSLiveSWTPM(install_image):
+    test_name = "incusos-live-swtpm"
+    test_seed = None
+
+    test_image, incusos_version = util._prepare_test_image(install_image, test_seed)
+
+    with IncusTestVM(test_name, test_image, root_size="1MiB") as vm:
+        # Remove the install image, enlarge its size and re-attach it
+        vm.RemoveDevice("boot-media")
+
+        subprocess.run(["/usr/bin/truncate", "--size", "50GiB", test_image], capture_output=True, check=True)
+
+        vm.AddDevice("live-image", "disk", "source="+test_image, "boot.priority=10")
+
+        # Remove the tpm
+        vm.RemoveDevice("vtpm")
+
+        # Start the VM and expect swtpm configuration to happen
+        vm.StartVM()
+        vm.WaitAgentRunning()
+        vm.WaitExpectedLog("incus-osd", "Configuring swtpm-backed TPM on first live boot, restarting in five seconds")
+        vm.LogDoesntContain("incus-osd", "Auto-generating encryption recovery key, this may take a few seconds")
+
+        # After auto-reboot, expect IncusOS to start running immediately
+        time.sleep(10)
+        vm.WaitAgentRunning()
+        vm.WaitExpectedLog("incus-osd", "Auto-generating encryption recovery key, this may take a few seconds")
+        vm.WaitExpectedLog("incus-osd", "No physical TPM found, using swtpm")
         vm.WaitExpectedLog("incus-osd", "Downloading application update application=incus version="+incusos_version)
         vm.WaitExpectedLog("incus-osd", "System is ready version="+incusos_version)
