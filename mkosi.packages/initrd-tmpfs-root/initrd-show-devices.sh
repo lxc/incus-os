48d903d5fdeb87db4d284a30d3b97cc218e66f02
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2025-12-10 09:46:24 -0700

    base: Add TPM info to debug output
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- mkosi.packages/initrd-tmpfs-root/initrd-show-devices.sh
+++ mkosi.packages/initrd-tmpfs-root/initrd-show-devices.sh
@@ -1,33 +1,46 @@
 #!/bin/sh
 
-# shellcheck disable=SC3000-SC4000
+# shellcheck disable=SC2028,SC2086
 
 while true; do
     for TTY in $TTYS; do
-        echo -e "\033cIncusOS failed to start. Debug information follows." > "$TTY" || true
+        echo "\033cIncusOS failed to start. Debug information follows." > "$TTY" || true
         echo "$ lsblk" > "$TTY" || true
         lsblk > "$TTY" || true
     done
     sleep 10
 
     for TTY in $TTYS; do
-        echo -e "\033cIncusOS failed to start. Debug information follows." > "$TTY" || true
+        echo "\033cIncusOS failed to start. Debug information follows." > "$TTY" || true
         echo "$ lscpi" > "$TTY" || true
         lspci > "$TTY" || true
     done
     sleep 10
 
     for TTY in $TTYS; do
-        echo -e "\033cIncusOS failed to start. Debug information follows." > "$TTY" || true
+        echo "\033cIncusOS failed to start. Debug information follows." > "$TTY" || true
         echo "$ lsusb" > "$TTY" || true
         lsusb > "$TTY" || true
     done
     sleep 10
 
     for TTY in $TTYS; do
-        echo -e "\033cIncusOS failed to start. Debug information follows." > "$TTY" || true
+        echo "\033cIncusOS failed to start. Debug information follows." > "$TTY" || true
         echo "$ ls -lh /sys/class/block/*/device/driver" > "$TTY" || true
         ls -lh /sys/class/block/*/device/driver > "$TTY" || true
+    done
+    sleep 10
+
+    for TTY in $TTYS; do
+        echo "\033cIncusOS failed to start. Debug information follows." > "$TTY" || true
+        echo "$ dmesg | grep -i tpm" > "$TTY" || true
+        dmesg | grep -i tpm > "$TTY" || true
+        echo "$ ls -lh /dev/tpm*" > "$TTY" || true
+        ls -lh /dev/tpm* > "$TTY" || true
+        echo "$ for i in /sys/class/tpm/*/tpm_version_major; do echo \"\$i => \$(cat $i)\"; done" > "$TTY" || true
+        for i in /sys/class/tpm/*/tpm_version_major; do
+            echo "$i => $(cat $i)" > "$TTY" || true;
+        done
     done
     sleep 10
 done
