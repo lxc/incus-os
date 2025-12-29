bf5da9931345494940b88583d5c0a6cbc2c5309f
Stéphane Graber <stgraber@stgraber.org>
2025-12-29 06:57:58 -0500

    incus-osd/api/service_linstor: Set omitempty on optional fields
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- incus-osd/api/service_linstor.go
+++ incus-osd/api/service_linstor.go
@@ -2,11 +2,11 @@ package api
 
 // ServiceLinstorConfig represents the Linstor service configuration.
 type ServiceLinstorConfig struct {
-	Enabled                bool     `json:"enabled"                  yaml:"enabled"`
-	ListenAddress          string   `json:"listen_address"           yaml:"listen_address"`
-	TLSServerCertificate   string   `json:"tls_server_certificate"   yaml:"tls_server_certificate"`
-	TLSServerKey           string   `json:"tls_server_key"           yaml:"tls_server_key"`
-	TLSTrustedCertificates []string `json:"tls_trusted_certificates" yaml:"tls_trusted_certificates"`
+	Enabled                bool     `json:"enabled"                            yaml:"enabled"`
+	ListenAddress          string   `json:"listen_address"                     yaml:"listen_address"`
+	TLSServerCertificate   string   `json:"tls_server_certificate,omitempty"   yaml:"tls_server_certificate,omitempty"`
+	TLSServerKey           string   `json:"tls_server_key,omitempty"           yaml:"tls_server_key,omitempty"`
+	TLSTrustedCertificates []string `json:"tls_trusted_certificates,omitempty" yaml:"tls_trusted_certificates,omitempty"`
 }
 
 // ServiceLinstorState represents state for the Linstor service.
