e6c115168f4ec49c453e659adebe7b81c1dfc9de
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2026-01-02 13:34:27 -0700

    incus-osd/api: Define an explicit ApplicationState struct
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- incus-osd/api/application.go
+++ incus-osd/api/application.go
@@ -7,13 +7,16 @@ import (
 // ApplicationConfig represents additional configuration for an application.
 type ApplicationConfig struct{}
 
+// ApplicationState represents the state of the application.
+type ApplicationState struct {
+	Initialized  bool       `json:"initialized"             yaml:"initialized"`
+	Version      string     `json:"version"                 yaml:"version"`
+	LastRestored *time.Time `json:"last_restored,omitempty" yaml:"last_restored,omitempty"` // In system's timezone.
+}
+
 // Application represents the state and configuration of a generic application.
 type Application struct {
-	State struct {
-		Initialized  bool       `json:"initialized"             yaml:"initialized"`
-		Version      string     `json:"version"                 yaml:"version"`
-		LastRestored *time.Time `json:"last_restored,omitempty" yaml:"last_restored,omitempty"` // In system's timezone.
-	} `json:"state" yaml:"state"`
+	State ApplicationState `json:"state" yaml:"state"`
 
 	Config ApplicationConfig `json:"config" yaml:"config"`
 }
