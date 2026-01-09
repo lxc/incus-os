17c95ae480d6a845d2c34694f147bb7bac5d149a
Stéphane Graber <stgraber@stgraber.org>
2026-01-09 09:50:10 -0500

    incus-osd/applications/gpu-support: Reload the modules on start
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- incus-osd/internal/applications/app_gpu_support.go
+++ incus-osd/internal/applications/app_gpu_support.go
@@ -1,9 +1,46 @@
 package applications
 
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/lxc/incus/v6/shared/subprocess"
+)
+
 type gpuSupport struct {
 	common
 }
 
 func (*gpuSupport) Name() string {
 	return "gpu-support"
+}
+
+func (*gpuSupport) Start(ctx context.Context, _ string) error {
+	// Reload the modules if loaded.
+	for _, module := range []string{"amdgpu", "i915", "nouveau"} {
+		// Check if loaded.
+		_, err := os.Stat("/sys/module/" + module)
+		if err != nil {
+			continue
+		}
+
+		// Unload the module.
+		_, err = subprocess.RunCommandContext(ctx, "/sbin/rmmod", module)
+		if err != nil {
+			slog.Warn("Failed to unload kernel module", "module", module)
+
+			continue
+		}
+
+		// Load the module back.
+		_, err = subprocess.RunCommandContext(ctx, "/sbin/modprobe", module)
+		if err != nil {
+			slog.Warn("Failed to load kernel module", "module", module)
+
+			continue
+		}
+	}
+
+	return nil
 }
