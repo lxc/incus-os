4d57180f7c72d1ec49d505bd1411adde2aa1e877
Stéphane Graber <stgraber@stgraber.org>
2026-03-04 10:37:19 -0500

    incus-osd/api: Introduce ServiceTailscaleConfig
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- incus-osd/api/service_tailscale.go
+++ incus-osd/api/service_tailscale.go
@@ -2,16 +2,18 @@ package api
 
 // ServiceTailscale represents the state and configuration of the Tailscale service.
 type ServiceTailscale struct {
-	State struct{} `json:"state" yaml:"state"`
+	Config ServiceTailscaleConfig `json:"config" yaml:"config"`
+	State  struct{}               `json:"state"  yaml:"state"`
+}
 
-	Config struct {
-		Enabled          bool     `json:"enabled"           yaml:"enabled"`
-		LoginServer      string   `json:"login_server"      yaml:"login_server"`
-		AuthKey          string   `json:"auth_key"          yaml:"auth_key"` //nolint:gosec
-		AcceptRoutes     bool     `json:"accept_routes"     yaml:"accept_routes"`
-		AdvertisedRoutes []string `json:"advertised_routes" yaml:"advertised_routes"`
-		ServeEnabled     bool     `json:"serve_enabled"     yaml:"serve_enabled"`
-		ServePort        int      `json:"serve_port"        yaml:"serve_port"`
-		ServeService     string   `json:"serve_service"     yaml:"serve_service"`
-	} `json:"config" yaml:"config"`
+// ServiceTailscaleConfig represents additional configuration for the Tailscale service.
+type ServiceTailscaleConfig struct {
+	Enabled          bool     `json:"enabled"           yaml:"enabled"`
+	LoginServer      string   `json:"login_server"      yaml:"login_server"`
+	AuthKey          string   `json:"auth_key"          yaml:"auth_key"` //nolint:gosec
+	AcceptRoutes     bool     `json:"accept_routes"     yaml:"accept_routes"`
+	AdvertisedRoutes []string `json:"advertised_routes" yaml:"advertised_routes"`
+	ServeEnabled     bool     `json:"serve_enabled"     yaml:"serve_enabled"`
+	ServePort        int      `json:"serve_port"        yaml:"serve_port"`
+	ServeService     string   `json:"serve_service"     yaml:"serve_service"`
 }
