8351feacfc2881a09d6c3c450a538599759be7b0
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2026-03-29 20:02:04 -0600

    incus-osd/systemd: If the system is shutting down, don't try to manually stop units
    When incus-osd receives SIGTERM, it attempts to stop all units for applications
    and services. When the system is already in the process of shutting down, for
    some reason trying to stop a socket unit hangs. Eventually the TimeoutStopSec
    expires, and incus-osd gets a SIGKILL. This is bad, since we cannot perform a
    final save of the state to disk and we introduce a delay in the poweroff/
    reboot cycle.
    
    I couldn't find a proper way to set systemd dependencies for the incus-osd
    service that didn't cause ordering cycles between stop dependencies, so instead
    rely on getting the system state from `systemd is-system-running` and if we're
    shutting down simply return without trying to stop the unit(s) ourselves.
    
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- incus-osd/internal/systemd/systemctl.go
+++ incus-osd/internal/systemd/systemctl.go
@@ -2,6 +2,7 @@ package systemd
 
 import (
 	"context"
+	"strings"
 
 	"github.com/lxc/incus/v6/shared/subprocess"
 )
@@ -50,6 +51,20 @@ func StopUnit(ctx context.Context, units ...string) error {
 	args := []string{"stop"} //nolint:prealloc
 	args = append(args, units...)
 
+	// If the system is currently shutting down, don't do anything. This addresses
+	// a tricky situation where the incus-osd service is stopped by systemd during a
+	// poweroff/reboot, and then we hang stopping a socket unit. Something seems to
+	// wedge during the shutdown process so that sockets will not stop. This then
+	// results in a hard SIGKILL to incus-osd and prevents a clean service shutdown.
+	//
+	// Attempting to set proper systemd dependencies for incus-osd such as
+	// After=sockets.target or Before=incus.socket result in ordering cycles between
+	// stop dependencies, and I haven't been able to figure out if it's actually
+	// possible to properly express this in a way systemd can handle automatically.
+	if IsSystemRunning(ctx) == "stopping" {
+		return nil
+	}
+
 	_, err := subprocess.RunCommandContext(ctx, "systemctl", args...)
 	if err != nil {
 		return err
@@ -76,4 +91,13 @@ func IsFailed(ctx context.Context, unit string) bool {
 	}
 
 	return result == "failed\n"
+}
+
+// IsSystemRunning returns the output of running `systemctl is-system-running` as described at
+// https://www.freedesktop.org/software/systemd/man/latest/systemctl.html#is-system-running.
+func IsSystemRunning(ctx context.Context) string {
+	// Ignore the error, since non-zero exit codes are expected.
+	result, _ := subprocess.RunCommandContext(ctx, "systemctl", "is-system-running")
+
+	return strings.TrimSuffix(result, "\n")
 }
