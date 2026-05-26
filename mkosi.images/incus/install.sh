e8161314d7765400b0623cee452483224da2a095
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2026-05-26 10:35:36 -0600

    mkosi.images/incus: Configure apt pinning for stable Incus packages
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- mkosi.images/incus/install.sh
+++ mkosi.images/incus/install.sh
@@ -4,11 +4,22 @@ set -eu
 
 [ "$1" = "final" ] || exit 0
 
-# Install the incus packages.
-apt-get update
-apt-get install incus incus-ui-canonical --yes
+# This is a terrible hack. mkosi doesn't support subimage-specific sandboxes,
+# which would be the most correct solution as we could cleanly define an apt
+# pinning configuration for this specific subimage. Alternatively, if apt
+# supported getting pinning configuration from the command line, we could
+# add that in a slightly-ugly apt-get command. However, we're stuck with creating
+# the pinning configuration under /run/, since the actual file system that
+# we have access to at this stage is read-only. It works, but I don't like it.
 
-# Install additional dependencies/recommends.
-apt-get install btrfs-progs xfsprogs --yes
+mkdir -p /run/apt/preferences.d/
+cat <<EOF > /run/apt/preferences.d/zabbly
+Package: *
+Pin: release l=incus-stable
+Pin-Priority: 900
+EOF
+
+# Install the incus packages.
+apt-get -o Dir::Etc::PreferencesParts="/run/apt/preferences.d/" install incus incus-ui-canonical --yes
 
 exit 0
