7b28173377abd0347191e94ebbf534f0a620ab01
Stéphane Graber <stgraber@stgraber.org>
2026-08-26 16:06:24 -0400

    incus-osd/api: Add journal upload logging options
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- incus-osd/api/system_logging.go
+++ incus-osd/api/system_logging.go
@@ -7,9 +7,18 @@ type SystemLoggingSyslog struct {
 	LogFormat string `json:"log_format" yaml:"log_format"`
 }
 
+// SystemLoggingJournalUpload contains the configuration options for uploading the journal to a remote server.
+type SystemLoggingJournalUpload struct {
+	URL                  string `json:"url"                    yaml:"url"`
+	TLSClientCertificate string `json:"tls_client_certificate" yaml:"tls_client_certificate"`
+	TLSClientKey         string `json:"tls_client_key"         yaml:"tls_client_key"`
+	TLSCACertificate     string `json:"tls_ca_certificate"     yaml:"tls_ca_certificate"`
+}
+
 // SystemLoggingConfig holds the modifiable part of the logging data.
 type SystemLoggingConfig struct {
-	Syslog SystemLoggingSyslog `json:"syslog" yaml:"syslog"`
+	JournalUpload SystemLoggingJournalUpload `json:"journal_upload" yaml:"journal_upload"`
+	Syslog        SystemLoggingSyslog        `json:"syslog"         yaml:"syslog"`
 }
 
 // SystemLoggingState represents state for the system's logging configuration.
