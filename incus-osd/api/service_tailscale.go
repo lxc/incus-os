16fc864a356535be666d05cb0772e76073714667
Stéphane Graber <stgraber@stgraber.org>
2026-08-27 18:27:57 -0400

    incus-osd/api: Add Tailscale listen port option
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- incus-osd/api/service_tailscale.go
+++ incus-osd/api/service_tailscale.go
@@ -15,6 +15,7 @@ type ServiceTailscaleConfig struct {
 	Enabled                bool     `json:"enabled"                    yaml:"enabled"`
 	LoginServer            string   `json:"login_server"               yaml:"login_server"`
 	AuthKey                string   `json:"auth_key"                   yaml:"auth_key"`
+	Port                   int      `json:"port"                       yaml:"port"`
 	AcceptRoutes           bool     `json:"accept_routes"              yaml:"accept_routes"`
 	AcceptDNS              bool     `json:"accept_dns"                 yaml:"accept_dns"`
 	AdvertisedRoutes       []string `json:"advertised_routes"          yaml:"advertised_routes"`
