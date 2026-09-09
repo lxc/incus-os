3f370bb9c7cf61a7399878c6e9c80c60ed077ba9
Stéphane Graber <stgraber@stgraber.org>
2026-09-09 17:06:31 -0400

    incus-ceph: Pin to 20.2.2
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- mkosi.images/incus-ceph/install.sh
+++ mkosi.images/incus-ceph/install.sh
@@ -6,6 +6,20 @@ set -eu
 
 # Install the packages.
 apt-get update
-apt-get install ceph-common --yes
+
+# FIXME: Pinning to older version to avoid breakage on arm64.
+version="20.2.2-pve1"
+apt-get install --yes "ceph-common=${version}" \
+    "libcephfs2=${version}" \
+    "librados2=${version}" \
+    "libradosstriper1=${version}" \
+    "librbd1=${version}" \
+    "librgw2=${version}" \
+    "python3-ceph-argparse=${version}" \
+    "python3-ceph-common=${version}" \
+    "python3-cephfs=${version}" \
+    "python3-rados=${version}" \
+    "python3-rbd=${version}" \
+    "python3-rgw=${version}"
 
 exit 0
